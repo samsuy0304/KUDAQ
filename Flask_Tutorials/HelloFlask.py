fa2369492eaefbcfbd758419664771e8625cc162
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2023-02-01 00:10:05 -0600

    Login_results
    Need to figure our how to use two different def in same route

--- Flask_Tutorials/HelloFlask.py
+++ Flask_Tutorials/HelloFlask.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python
 
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
+@app.route('/success/<name>')
+def success(name):
+   return render_template('Profile.html', name = name)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World'
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
